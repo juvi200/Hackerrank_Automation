53cfa40cb3bbf55739033a921a45fedc412657bd
juvi200 <68636170+juvi200@users.noreply.github.com>
2021-10-26 19:47:50 +0530

    updated file

--- HackerRankAutomation.js
+++ HackerRankAutomation.js
@@ -1,9 +1,5 @@
 //node HackerRankAutomation.js --url=https://www.hackerrank.com/ --config=config.json
 
-//npm init -y
-//npm install minimist
-//
-
 let puppeteer=require("puppeteer");
 let fs=require("fs");
 let minimist=require("minimist");
@@ -56,55 +52,75 @@ async function run(){
     //click manage contests
     //await page.waitForSelector("a[href='/administration/contests/']");
     await page.click("a[href='/administration/contests/']");
-    console.log("till administration");
-
-    //find pages
-    //await page.waitForSelector("a[data-attr1='last']");
-    // let numpages=await page.$eval("a[data-attr1='last']",function(atag)
-    // {
-    //     let np=parseInt(atag.getAttribute('data-page'));
-    //     return np;
-    // });
-    // console.log(numpages);
-
-//     //click on contest
-//     await page.waitForSelector("p.mmT");
-//     await page.click("p.mmT");
-//     //wait 3 sec to load n pop up go
-//    await page.waitFor(3000);
-// //click on moderators
-//    await page.waitForSelector("li[data-tab='moderators']");
-//    await page.click("li[data-tab='moderators']");
-//    //write moderator
-//    await page.waitForSelector("input#moderator");
-//    await page.type("input#moderator",configJSO.moderator,{delay:50});
-// //press enter after adding moderator
-//    await page.keyboard.press('Enter');
-   
-
-//find number of pages total
-// await page.waitForSelector("a[data-attr1='last']");
-// let nump=await page.$eval("a[data-attr1='last']",function(lastTag)
-// {
-//     let np=parseInt(getAttribute("data-page"));
-//     return np;
-// });
-// console.log(numpages);
- 
-
-
-
-
-
-
-
-
-   //function to add moderator
-//    async function saveModerator(tab,href,moderator)
-// {
     
-// }
+    await page.waitForSelector("a[data-attr1='Last']");
+    let numpages=await page.$eval("a[data-attr1='Last']",function(atag)
+    {
+        let np=parseInt(atag.getAttribute('data-page'));
+        return np;
+    });
+    console.log(numpages);
+
+
+//move through all pages
+for(i=0;i<numpages;i++)
+{
+    await handlePage(browser,page);
+    
+}
+
+async function handlePage(browser,page)
+{
+
+    await page.waitForSelector("a.backbone.block-center");
+    let curls=await page.$$eval("a.backbone.block-center",function(atags){
+        let urls=[];
+        for(let i=0;i<atags.length;i++)
+        {
+            let url=atags[i].getAttribute("href");
+            urls.push(url);
+        }
+        return urls;
+    });
+
+    //now in curls all urls r present,now handle 1 contest through 1 url
+   for(let i=0;i<curls.length;i++)
+   {
+       await handlecontest(browser,page,curls[i]);
+   }
+
+    //move to next page
+    await page.waitFor(1500);
+    await page.waitForSelector("a[data-attr1='Right']");
+    await page.click("a[data-attr1='Right']");
+}
+
+
+async function handlecontest(browser,page,curl)
+{
+    //now open new tab on new tab
+    let npage=await browser.newPage();
+
+    await npage.goto(args.url+curl);
+    await npage.waitFor(2000);
+    //click on moderator
+    await npage.waitForSelector("li[data-tab='moderators']");
+    await npage.click("li[data-tab='moderators']");
+//write moderator name
+    await npage.waitForSelector("input#moderator");
+    await npage.type("input#moderator",configJSO.moderator,{delay:50});
+    //press enter
+     await npage.keyboard.press('Enter');
+     await npage.waitFor(1000);
+    await npage.close();
+    await page.waitFor(2000);
+}
+
+
+  
 }
 run();
+
+
 
 
